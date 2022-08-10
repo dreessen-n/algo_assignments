3acb2404fa9e589827c09a6fc3123efeabf49f8c
dreessen-n <dreessen.edu@gmail.com>
2022-08-10 16:42:49 -0600

    Add assignment opional contains

--- singlyLinkedList/contains.js
+++ singlyLinkedList/contains.js
@@ -1,4 +1,4 @@
-class Node {
+class sLLNode {
     constructor(data) {
         this.data = data;
         this.next = null;              
@@ -7,6 +7,15 @@ class Node {
 class singlyLinkedList {
     constructor() {
         this.head = null;
+    }
+    addFront(data) {
+        var new_node = new sLLNode(data); // create new instance of the sLLNode
+        // Assign the current head to the next reference of the new_node
+        new_node.next = this.head; 
+        // Now set the new_node as the head
+        this.head = new_node;
+        // Could also return just 'this' rather than 'this.head'
+        return this.head;
     }
     contains(data) {
         let runner=this.head;
@@ -20,6 +29,29 @@ class singlyLinkedList {
     }
 }
 
-contains(4);
+// Create an empty singlyLinkedList
+var mySLL = new singlyLinkedList();
+
+// Add some values to the ssl
+mySLL.addFront(1);
+mySLL.addFront(2);
+mySLL.addFront(3);
+mySLL.addFront(4);
+mySLL.addFront(5);
+
+// Should return true
+console.log(mySLL.contains(2));
+
+// Should return true
+console.log(mySLL.contains(4));
+
+// Should return false
+console.log(mySLL.contains(8));
+
+// Should return false
+console.log(mySLL.contains(-2));
+
+// Should return false
+console.log(mySLL.contains(13));
 
 
