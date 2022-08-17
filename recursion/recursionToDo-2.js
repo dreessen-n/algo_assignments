1b041d580800f782a23e11b95f0fadb81ea206e4
dreessen-n <dreessen.edu@gmail.com>
2022-08-17 15:12:09 -0600

    Add assignment

---
+++ recursion/recursionToDo-2.js
@@ -0,0 +1,11 @@
+// Recursive Fibonacci
+// Write rFib(num). Recursively compute and return numth Fibonacci value. As earlier, treat first two (num = 0, num = 1) Fibonacci vals as 0 and 1. Examples: 
+// rFib(2) = 1 (0+1); 
+// rFib(3) = 2 (1+1); 
+// rFib(4) = 3 (1+2); 
+// rFib(5) = 5 (2+3).rFib(3.65) = rFib(3) = 2, rFib(-2) = rFib(0) = 0.
+
+function recursiveFibonacci() {
+    
+}
+
