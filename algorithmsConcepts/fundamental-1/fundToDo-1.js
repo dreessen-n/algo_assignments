a0626bc170f0236ebba2602758fd35664d3226a4
dreessen-n <dreessen.edu@gmail.com>
2022-08-20 18:13:11 -0600

    Update assignment

--- algorithmsConcepts/fundamental-1/fundToDo-1.js
+++ algorithmsConcepts/fundamental-1/fundToDo-1.js
@@ -88,3 +88,98 @@ leapYear(1999);
 leapYear(2024);
 leapYear(2028);
 leapYear(1900);
+
+// Print and Count
+// Print all integer multiples of 5, from 512 to 4096. Afterward, also log how many there were.
+
+function printCount() {
+    var count = 0;
+    for (var i = 512; i <= 4096; i++) {
+        if ( i % 5 == 0) {
+            console.log(i);
+            count += 1;
+        }
+    }
+    console.log('There are ' + i + ' multiples of 5');
+}
+
+printCount();
+
+// Multiples of Six
+// Print multiples of 6 up to 60,000, using a WHILE.
+
+function mult6() {
+    var i = 0;
+    while (i < 60001) {
+        if (i % 6 == 0) {
+            console.log(i);
+        }
+        i++;
+    }
+}
+
+mult6();
+
+// Counting, the Dojo Way
+// Print integers 1 to 100. If divisible by 5, print "Coding" instead. If by 10, also print " Dojo".
+
+function dojoWay() {
+    for (var i = 1; i < 101;  i++) {
+        if ((i % 5 == 0) && (i % 10 == 0)) {
+            console.log('Coding Dojo');
+        } else if (i % 5 == 0) {
+            console.log('Coding');
+        } else {
+        console.log(i);
+        }
+    }
+}
+
+dojoWay();
+
+// What Do You Know?
+// Your function will be given an input parameter incoming. Please console.log this value.
+
+function printParameter(val) {
+    console.log(val);
+}
+
+printParameter('hello');
+printParameter(6);
+
+// Whoa, That Sucker’s Huge…
+// Add odd integers from -300,000 to 300,000, and console.log the final sum. Is there a shortcut?
+
+function suckerHuge() {
+    sum = 0;
+    var i = -300000;
+    while (i < 300001) {
+       if (i % 2 == 0) {
+           i += 1;
+       } else {
+           sum += i;
+           i += 1;
+       }
+    }
+    console.log(sum);
+}
+
+suckerHuge();
+
+// Countdown by Fours
+// Log positive numbers starting at 2016, counting down by fours (exclude 0), without a FOR loop.
+
+function countdown4() {
+    var i = 2016;
+    while (i != 0) {
+        console.log(i);
+        i -= 4;
+    }
+}
+
+countdown4();
+
+// Flexible Countdown
+// Based on earlier “Countdown by Fours”, given lowNum, highNum, mult, print multiples of mult from highNum down to lowNum, using a FOR. For (2,9,3), print 9 6 3 (on successive lines).
+
+
