009100f2a4846987d1669e00d5328c8d3634a823
dreessen-n <dreessen.edu@gmail.com>
2022-08-29 10:42:57 -0600

    Finish Fundamentals To Do 1

--- algorithmsConcepts/fundamental-1/fundToDo-1.js
+++ algorithmsConcepts/fundamental-1/fundToDo-1.js
@@ -182,4 +182,25 @@ countdown4();
 // Flexible Countdown
 // Based on earlier “Countdown by Fours”, given lowNum, highNum, mult, print multiples of mult from highNum down to lowNum, using a FOR. For (2,9,3), print 9 6 3 (on successive lines).
 
+function flexCountdown(lowNum, highNum, mult) {
+    for (var i = highNum; i > lowNum; i = i - 3) {
+        console.log(i);
+    }
+}
+
+flexCountdown(2, 9, 3);
+
+// The Final Countdown
+// This is based on “Flexible Countdown”. The parameter names are not as helpful, but the problem is essentially identical; don’t be thrown off! Given 4 parameters (param1,param2,param3,param4), print the multiples of param1, starting at param2 and extending to param3. One exception: if a multiple is equal to param4, then skip (don’t print) it. Do this using a WHILE. Given (3,5,17,9), print 6,12,15 (which are all of the multiples of 3 between 5 and 17, and excluding the value 9).
+
+function finalCountdown(par1, par2, par3, par4) {
+    for (var i = par3; i > par2; i--) {
+        if (i != par4 && i % par1 == 0) {
+            console.log(i);
+        }
+    }
+}
+
+finalCountdown(3, 5, 17, 9);
+
 
